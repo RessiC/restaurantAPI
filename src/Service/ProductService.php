20b6334510ae653d3847891a6a4bc1e6a81767fd
ressic <ressi.cyril@gmail.com>
2022-05-25 15:33:02 +0200

    productController

---
+++ src/Service/ProductService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Restaurant\Product;
+use App\Entity\Restaurant\Restaurant;
+use App\Repository\Restaurant\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class ProductService
+{
+    private ProductRepository $productRepository;
+    private ManagerRegistry $managerRegistry;
+
+
+    public function __construct(ProductRepository $productRepository, ManagerRegistry $managerRegistry)
+    {
+        $this->productRepository = $productRepository;
+        $this->managerRegistry = $managerRegistry;
+    }
+
+
+    public function createProductForRestaurant(Product $product, Restaurant $restaurant)
+    {
+        $product->setRestaurant($restaurant);
+
+        $entityManager = $this->managerRegistry->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+    }
+
+    public function editProductForRestaurant(Product $product, Restaurant $restaurant)
+    {
+
+    }
+}
